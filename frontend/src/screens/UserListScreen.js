9ac2a0482ed9d55b6b0724f9c3767306c6f1c96f
pulkitbest <pulkitgupta38@gmail.com>
2023-03-26 11:51:58 +0530

    User List is made using BootstrapTable next

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -1,10 +1,13 @@
 import React, {useEffect} from 'react'
 import {Link} from 'react-router-dom'
-import {Table, Button, Row, Col} from 'react-bootstrap'
+import BootstrapTable from 'react-bootstrap-table-next'
+import paginationFactory from 'react-bootstrap-table2-paginator'
+import filterFactory, {textFilter} from 'react-bootstrap-table2-filter'
 import {useDispatch, useSelector} from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { listUsers, deleteUser } from '../actions/userActions'
+import { Button, ButtonGroup, Col, Row } from 'react-bootstrap'
 
 const UserListScreen = ({history}) => {
     const dispatch = useDispatch()
@@ -25,11 +28,6 @@ const UserListScreen = ({history}) => {
             history.push('/login')
         }
     }, [dispatch, history, userInfo, successDelete])
-
-    const deleteHandler = (id) => {
-        if(window.confirm('Are you sure?'))
-        dispatch(deleteUser(id))
-    }
 
     const exportHandler = () => {
         const rows = [
@@ -80,6 +78,99 @@ const UserListScreen = ({history}) => {
         link.click()
     }
 
+    const deleteHandler = (id) => {
+        if(window.confirm('Are you sure?'))
+        dispatch(deleteUser(id))
+    }
+
+    const emailFormatter = (data) => {
+        return <a href={`mailto:${data}`}>{data}</a>
+    }
+
+    const cvFormatter = (data) => {
+        return <Link to={`/cv/${data}`}><i class='fa-solid fa-link'></i></Link>
+    }
+
+    const performFormatter = (data) => {
+        return (
+            <ButtonGroup>
+                <Button variant='dark' className='btn-sm' onClick={() => history.push(`/admin/user/${data}/edit`)}>
+                    <i className='fas fa-edit'></i>
+                </Button>
+                <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(data)}>
+                    <i className='fas fa-trash'></i>
+                </Button>
+            </ButtonGroup>
+        )
+    }
+
+    const isAdminFormatter = (data) => {
+        if(data === true){
+            return <i className='fas fa-check' style={{color: 'green'}}></i>
+        }
+        return <i className='fas fa-times' style={{color: 'red'}}></i>
+    }
+
+    const columns = [
+        {
+            dataField: 'rollNumber',
+            text: '',
+            sort: true,
+            filter: textFilter({
+                delay: 1000,
+                className: 'fw-bold',
+                placeholder: 'ROLL NUMBER',
+            }),
+        },
+        {
+            dataField: 'name',
+            text: '',
+            filter: textFilter({
+                delay: 1000,
+                className: 'fw-bold',
+                placeholder: 'NAME',
+            }),
+        },
+        {
+            dataField: 'email',
+            text: '',
+            filter: textFilter({
+                delay: 1000,
+                className: 'fw-bold',
+                placeholder: 'EMAIL',
+            }),
+            formatter: emailFormatter,
+        },
+        {
+            dataField: 'phone',
+            text: '',
+            filter: textFilter({
+                delay: 1000,
+                className: 'fw-bold',
+                placeholder: 'PHONE',
+            }),
+        },
+        {
+            dataField: '_id',
+            text: 'CV',
+            formatter: cvFormatter,
+        },
+        {
+            dataField: 'isAdmin',
+            text: 'ADMIN',
+            formatter: isAdminFormatter,
+        },
+        {
+            dataField: '_id',
+            text: 'EDIT/DELETE',
+            formatter: performFormatter,
+        }
+    ]
+
+    const defaultSorted = [{
+        dataField: 'rollNumber',
+        order: 'asc'
+    }]
 
     return (
         <>
@@ -93,47 +184,21 @@ const UserListScreen = ({history}) => {
                     </Button>}
                 </Col>
             </Row>
-            {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : (
-                <>
-                <Table striped bordered hover responsive className='table-sm'>
-                    <thead>
-                        <tr>
-                            <th class="th-sm">ROLL NO.</th>
-                            <th class="th-sm">NAME</th>
-                            <th class="th-sm">EMAIL</th>
-                            <th class="th-sm">COLLEGE EMAIL</th>
-                            <th class="th-sm">CV</th>
-                            <th class="th-sm">ADMIN</th>
-                            <th class="th-sm">EDIT/DELETE</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map(user => (
-                            <tr key={user._id}>
-                                <td>{user.rollNumber}</td>
-                                <td>{user.name}</td>
-                                <td><a href={`mailto:${user.email}`}>{user.email}</a></td>
-                                <td><a href={`mailto:${user.collegeEmail}`}>{user.collegeEmail}</a></td>
-                                <td><Link to={`/cv/${user._id}`}><i class='fa-solid fa-link'></i></Link></td>
-                                <td>{user.isAdmin ? <i className='fas fa-check' style={{color: 'green'}}></i> : (
-                                    <i className='fas fa-times' style={{color: 'red'}}></i>
-                                )}</td> 
-                                <td>
-                                    <Link to={`/admin/user/${user._id}/edit`}>
-                                        <Button variant='dark' className='btn-sm'>
-                                            <i className='fas fa-edit'></i>
-                                        </Button>
-                                    </Link>    
-                                    <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(user._id)}>
-                                        <i className='fas fa-trash'></i>
-                                    </Button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-                </>
-            )}
+            {loading && <Loader/>}
+            {error && <Message>{error}</Message>}
+            {users && 
+                <BootstrapTable 
+                    bootstrap4
+                    keyField='_id' 
+                    data={users} 
+                    columns={columns} 
+                    striped
+                    condensed
+                    pagination={paginationFactory()}
+                    filter={filterFactory()}
+                    defaultSorted={defaultSorted}
+                />
+            }
         </>
     )
 }
